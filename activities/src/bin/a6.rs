4c0d1cb722e5acf14dad8b6b9d47c1a8b9efe003
Pat Palmerston <patpalmerston@gmail.com>
2022-07-27 14:30:38 -0700

    a6

--- activities/src/bin/a6.rs
+++ activities/src/bin/a6.rs
@@ -10,4 +10,20 @@
 // * Print the variable within the while loop
 // * Do not use break to exit the loop
 
-fn main() {}
+fn main() {
+  // Program requirements:
+// * Counts down from 5 to 1, displays the countdown
+//   in the terminal, then prints "done!" when complete.
+//
+// Notes:
+// * Use a mutable integer variable
+  let mut i = 5;
+// * Use a while statement
+  while i >= 1 {
+// * Print the variable within the while loop
+// * Do not use break to exit the loop
+    println!("{:?}", i);
+    i = i - 1;
+  }
+
+}
