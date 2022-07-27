4d4e908ad0c6f1991b3563a0de51f7b0a20d727c
Pat Palmerston <patpalmerston@gmail.com>
2022-07-27 11:35:51 -0700

    a2

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,4 +8,20 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+// fn sub(a: i32, b: i32) -> i32 {
+//   a - b
+// }
+
+fn sum(a: i32, b: i32) -> i32 {
+  a + b
+}
+
+fn display(result: i32) {
+  println!("{:?}", result);
+}
+
+
+fn main() {
+  let result = sum(1, 2);
+  display(result);
+}
