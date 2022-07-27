f9e598aa55aad374cd55ec8ed4bbc2f9b7cafb95
Pat Palmerston <patpalmerston@gmail.com>
2022-07-27 12:52:12 -0700

    a5

--- activities/src/bin/a5.rs
+++ activities/src/bin/a5.rs
@@ -9,4 +9,19 @@
 // * Print the variable within the loop statement
 // * Use break to exit the loop
 
-fn main() {}
+
+fn main() {
+  // Notes:
+// * Use a mutable integer variable
+let mut i = 1;
+// * Use a loop statement
+  loop {
+  // * Print the variable within the loop statement
+    println!("{:?}", i);
+    i = i + 1;
+    if i == 5 {
+      // * Use break to exit the loop
+      break;
+    }
+  }
+}
