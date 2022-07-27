a4f0cf255cff02987353171dd252c22c3fb57d85
Pat Palmerston <patpalmerston@gmail.com>
2022-07-27 12:07:32 -0700

    a3b

--- activities/src/bin/a3b.rs
+++ activities/src/bin/a3b.rs
@@ -9,4 +9,14 @@
 // * Use an if..else if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+  let display_variable = 5;
+
+  if display_variable > 5 {
+    println!(">5")
+  } else if display_variable < 5 {
+    println!("<5")
+  } else {
+    println!("=5")
+  }
+}
