d22e378898dec6467241e5c4f332457f45344135
Pat Palmerston <patpalmerston@gmail.com>
2022-07-29 11:04:30 -0700

    what is ownership1

--- activities/src/bin/rust_docs_play.rs
+++ activities/src/bin/rust_docs_play.rs
@@ -1,4 +1,5 @@
-fn main() {
+//........Rust Docs Ownership portion...........//
+// fn main() {
   //.......ownership............//
   // s is not valid as it has not been declared
   // let s = "hello"; // s is valid from this point on
@@ -16,31 +17,31 @@ fn main() {
   // println!("s1 = {}, s2 = {}", s1, s2) // s2 is a clone of s1
 
   //.... ownership rules .... //
-  let s = String::from("hello"); // s comes into scope
+  // let s = String::from("hello"); // s comes into scope
 
-  takes_ownership(s);           // s's value moves into the function...
+  // takes_ownership(s);           // s's value moves into the function...
                                 // ... and so is no longer valid here
 
   // println!("{}", s); // creates error value borrowed after move
 
-  let x = 5;                    // x comes into scope
+  // let x = 5;                    // x comes into scope
 
-  makes_copy(x);                // x would move into the function,
+  // makes_copy(x);                // x would move into the function,
                                 // but i32 is Copy, so it's okay to still
                                 // use x afterward
-  makes_copy(x) // adds another copy
+  // makes_copy(x) // adds another copy
 
-} // s is now out of scope
+// } // s is now out of scope
   // Here, x goes out of scope, then s. But because s's value was moved, nothing
   // special happens.
 
-fn takes_ownership(some_string: String) { // some_string comes into scope
-  println!("{}", some_string);
-} // Here, some_string goers out of scope and 'drop' is called. The backing
+// fn takes_ownership(some_string: String) { // some_string comes into scope
+  // println!("{}", some_string);
+// } // Here, some_string goers out of scope and 'drop' is called. The backing
   // memory is freed
 
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
-  println!("{}", some_integer)
-} // Here, some_integer goes out of scope. Nothing special happens.
+// fn makes_copy(some_integer: i32) { // some_integer comes into scope
+  // println!("{}", some_integer)
+// } // Here, some_integer goes out of scope. Nothing special happens.
 
 // makes_copy(x); //out of scope and stalls whole terminal run
